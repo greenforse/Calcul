7b005095b751843144a326f9390c6e82ee470f46
Kosyuchenko Anton <greenforse1996@gmail.com>
2021-05-20 18:25:45 +0500

    1234

--- State1.py
+++ State1.py
@@ -2,4 +2,9 @@ from State import State
 class State1(State):
 
     def input_digit(self, digit):
-        self.calculator.addDigit(digit)
+        while digit !="+" or digit !="-" or digit != "*" or digit !="/" or digit!="=":
+            self.calculator.addDigit(digit)
+        if digit = "=":
+            self.calculator.calculate()
+        else:
+            self.calculator.setState(State2(self.calculator))
