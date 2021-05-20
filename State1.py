db59a4ea5281025080011fc97936d5b2b12e7395
Kosyuchenko Anton <greenforse1996@gmail.com>
2021-05-20 17:38:44 +0500

    Старт

---
+++ State1.py
@@ -0,0 +1,16 @@
+class State1():
+    
+    def run(self):
+        try:
+            i=int(input(">"))
+        except:
+            
+        #print()
+        #i = input("> ")
+        #a=[]
+        #a.append(i)
+        #sum=0
+        #for j in range(len(a)):
+        #    result=a[j]*(10**((len(a))-(j+1)))
+        #    sum += result
+        #return sum
---
+++ main.py
@@ -0,0 +1,5 @@
+from State1 import State1
+
+st = State1()
+r = st.run()
+print(r)
